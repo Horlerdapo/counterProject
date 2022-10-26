5bd4fff45434203896e562183ee734966602001d
unknown <topdapson@gmail.com>
2022-10-26 15:24:42 -0600

    added javascript

---
+++ index.js
@@ -0,0 +1,7 @@
+let num = 0;
+const value = document.querySelector("#inputValue");
+const plus = document.querySelector(".increase-button");
+const minus = document.querySelector(".decrease-button");
+const reset = document.querySelector(".reset-button");
+const input = document.querySelector("input");
+
